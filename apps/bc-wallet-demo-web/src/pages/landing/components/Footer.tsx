21132d77424242b8067091f9fce962c18d14038b
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2025-08-15 16:34:10 -0700

    initial clean up for landing page

--- apps/bc-wallet-demo-web/src/pages/landing/components/Footer.tsx
+++ apps/bc-wallet-demo-web/src/pages/landing/components/Footer.tsx
@@ -10,10 +10,10 @@ export const Footer: React.FC = () => {
   return (
     <div>
       <div className="flex dark:text-white justify-center content-center mt-2 select-none">
-        <a href="mailto:ditrust@gov.bc.ca">ditrust@gov.bc.ca</a>
+        <a href="mailto:trustservices@quartech.com">trustservices@quartech.com</a>
       </div>
       <div className="flex dark:text-white justify-center content-center select-none">
-        <p className="self-center mr-2 text-sm">Copyright &#169; 2022 Government of British Columbia</p>
+        <p className="self-center mr-2 text-sm">Copyright &#169; 2025 Quartech</p>
       </div>
     </div>
   )
